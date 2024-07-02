8c0e164f9c01895511fce2b90848f8f1f47182f6
alex-delia <adelia3@uwo.ca>
2024-07-02 12:32:10 -0400

    change var to const, add cors, and include new routes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var postsRouter = require('./routes/posts');
+const indexRouter = require('./routes/index');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -17,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
+app.use('/comments', commentsRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
