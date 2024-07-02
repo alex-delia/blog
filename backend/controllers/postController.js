f16391e64b8ddf625fd9b149bf1af68465771b32
alex-delia <adelia3@uwo.ca>
2024-07-02 12:39:32 -0400

    modify default sends for posts

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -10,20 +10,20 @@ exports.posts_list = asyncHandler(async (req, res, next) => {
 
 //display individual post on GET
 exports.post_detail = asyncHandler(async (req, res, next) => {
-    res.send('GET request for individual post');
+    res.send(`GET request for individual post: ${req.params.postId}`);
 });
 
 //create post on POST
 exports.post_create = asyncHandler(async (req, res, next) => {
-    res.send('POST request for individual post');
+    res.send(`POST request for new post`);
 });
 
 //update post on PUT
 exports.post_update = asyncHandler(async (req, res, next) => {
-    res.send('PUT request for individual post');
+    res.send(`PUT request for updating post: ${req.params.postId}`);
 });
 
 //delete post on DELETE
 exports.post_delete = asyncHandler(async (req, res, next) => {
-    res.send('DELETE request for individual post');
+    res.send(`DELETE request for deleting post: ${req.params.postId}`);
 });
