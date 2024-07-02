5b6ee97592eb5cbfe2c000a0c46f2e7e431b3792
alex-delia <adelia3@uwo.ca>
2024-07-02 08:14:20 -0400

    initial commit

---
+++ backend/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+    },
+    accountType: {
+        type: String,
+        required: true,
+        default: 'viewer',
+        enum: ['viewer', 'author'],
+    }
+});
+
+userSchema.virtual('fullname').get(function () {
+    return `${this.firstName} ${this.lastName}`;
+});
+
+
+module.exports = mongoose.model('User', userSchema);
