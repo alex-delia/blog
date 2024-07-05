4703060300394e8138604b85ea73a88bb014a98f
alex-delia <adelia3@uwo.ca>
2024-07-05 17:59:55 -0400

    create controllers to send Authors and author detail

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,14 +3,27 @@ const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require("express-validator");
 
-//GET user details
+//GET author details
 exports.author_detail = asyncHandler(async (req, res, next) => {
-    res.send(`GET request to author user: ${req.params.authorId}`);
+    const author = await User.findById(req.params.authorId, 'firstName lastName').exec();
+
+    if (author === null) {
+        // No results.
+        const err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.json(author);
 });
 
 //GET user details
 exports.author_list = asyncHandler(async (req, res, next) => {
-    res.send(`GET request for authors list`);
+    const allAuthors = await User.find({ accountType: 'author' }, 'firstName lastName')
+        .sort({ firstName: 1 })
+        .exec();
+
+    res.json(allAuthors);
 });
 
 //create user on POST
