4f520396bbd4b919d03657071161118439206746
alex-delia <adelia3@uwo.ca>
2024-07-05 16:03:00 -0400

    create a route to get comments

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -3,6 +3,10 @@ const Comment = require('../models/comment');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require("express-validator");
 
+exports.post_comments = asyncHandler(async (req, res, next) => {
+    res.send('GET request for displaying post comments');
+});
+
 //create comment on POST
 exports.comment_create = asyncHandler(async (req, res, next) => {
     res.send('POST request for new comment');
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const comment_controller = require('../controllers/commentController');
 
+//GET request for comments on a post
+router.get('/', comment_controller.post_comments);
+
 //POST request for creating comment
 router.post('/', comment_controller.comment_create);
 
