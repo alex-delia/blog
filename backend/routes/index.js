f6ec994c3500bf27e34bac3443753733bb36ceb6
alex-delia <adelia3@uwo.ca>
2024-07-05 23:27:35 -0400

    create route to login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.send('GET request for index page');
 });
 
+router.post('/login', function (req, res, next) {
+  res.send('GET request for index page');
+});
+
 module.exports = router;
