8f9311b0c3365b7884655c2660e9c5251658328a
alex-delia <adelia3@uwo.ca>
2024-07-05 21:42:27 -0400

    create user in database with POST route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 //GET author details
 exports.author_detail = asyncHandler(async (req, res, next) => {
@@ -27,9 +28,75 @@ exports.author_list = asyncHandler(async (req, res, next) => {
 });
 
 //create user on POST
-exports.user_create = asyncHandler(async (req, res, next) => {
-    res.send('POST request for new user');
-});
+exports.user_create = [
+    body('firstName')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('First Name must be specified.')
+        .escape(),
+    body('lastName')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Last Name must be specified.')
+        .escape(),
+    body('email')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Email must be specified.')
+        .isEmail()
+        .withMessage('Email format is invalid.')
+        .custom(async value => {
+            const user = await User.findOne({ email: value });
+            if (user) {
+                throw new Error('E-mail already in use');
+            }
+        })
+        .escape(),
+    body('password')
+        .trim()
+        .isStrongPassword()
+        .withMessage('Password must be at least 8 characters and include: 1 Uppercase, 1 Lowercase, 1 Number, 1 Symbol')
+        .escape(),
+    body('confirmPassword')
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password || value === '') {
+                throw new Error('Password\'s must match');
+            }
+            return true;
+        })
+        .escape(),
+
+    // Process request after validation and sanitization.
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const user = new User({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/errors messages.
+            res.status(400).json(errors.array());
+            return;
+        }
+
+        // Data from form is valid.
+        // Save user.
+        await user.save();
+        res.json({
+            message: "User Created",
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.email,
+        });
+    })
+];
 
 //delete user on DELETE
 exports.user_delete = asyncHandler(async (req, res, next) => {
