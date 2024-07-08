494c0ffabe3c0d287fa0b9ce9f9e43519b8b2e9d
alex-delia <adelia3@uwo.ca>
2024-07-07 20:08:19 -0400

    allow users to create comments when logged in

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -6,13 +6,57 @@ const { body, validationResult } = require("express-validator");
 //GET all comments for a post
 exports.post_comments = asyncHandler(async (req, res, next) => {
     const comments = await Comment.find({ post: req.params.postId }).populate('user');
-    res.json({ comments });
+
+    if (comments.length === 0) {
+        const err = new Error("No Comments found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.json({ data: comments });
 });
 
 //create comment on POST
-exports.comment_create = asyncHandler(async (req, res, next) => {
-    res.send('POST request for new comment');
-});
+exports.comment_create = [
+    body('text')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Comment text must be specified')
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            const err = new Error('Form Data is invalid');
+            err.status = 400;
+            err.details = errors.array();
+            return next(err);
+        }
+
+        console.log(req.params);
+
+        const comment = new Comment({
+            text: req.body.text,
+            user: req.user.id,
+            post: req.params.postId
+        });
+
+        await comment.populate({
+            path: 'user',
+            select: 'firstName lastName'
+        });
+
+        await comment.populate({
+            path: 'post',
+            select: 'title'
+        });
+
+        await comment.save();
+
+        res.json({ message: 'Comment made successfully', data: comment });
+    })
+];
 
 //delete comment on POST
 exports.comment_delete = asyncHandler(async (req, res, next) => {
--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const comment_controller = require('../controllers/commentController');
+const { authenticateJWT } = require('../middlewares/authMiddleware');
 
 //GET request for comments on a post
-router.get('/', comment_controller.post_comments);
+router.get('/:postId/comments', comment_controller.post_comments);
 
 //POST request for creating comment
-router.post('/', comment_controller.comment_create);
+router.post('/:postId/comments', authenticateJWT, comment_controller.comment_create);
 
 //DELETE request for deleting comment
-router.delete('/:commentId', comment_controller.comment_delete);
+router.delete(':postId/comments/:commentId', comment_controller.comment_delete);
 
 module.exports = router;
