809fcf3b92d1c1028cf46c0c45553c3950a6016d
alex-delia <adelia3@uwo.ca>
2024-07-09 15:29:08 -0400

    create routes for React SPA

--- frontend-user/src/main.jsx
+++ frontend-user/src/main.jsx
@@ -1,10 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import About from './components/pages/About.jsx';
+import Home from './components/pages/Home.jsx';
+import Authors from './components/pages/Authors.jsx';
+import Posts from './components/pages/Posts.jsx';
+import Post from './components/pages/Post.jsx';
+import Login from './components/pages/Login.jsx';
+import Register from './components/pages/Register.jsx';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from "./error-page";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/authors',
+        element: <Authors />
+      },
+      {
+        path: '/posts',
+        element: <Posts />
+      },
+      {
+        path: '/posts/:postId',
+        element: <Post />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
