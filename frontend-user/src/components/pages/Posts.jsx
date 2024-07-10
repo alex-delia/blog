1f73c2b0a2497d2abf679e28956936f449cb36b9
alex-delia <adelia3@uwo.ca>
2024-07-10 01:49:38 -0400

    fetch posts data with swr and improve post previews

--- frontend-user/src/components/pages/Posts.jsx
+++ frontend-user/src/components/pages/Posts.jsx
@@ -1,48 +1,39 @@
-import { useState, useEffect } from "react";
+import useSWR from 'swr';
+import { axiosFetch } from '../../helpers/fetch';
+import { DateTime } from "luxon";
+import { Link } from "react-router-dom";
 
 const usePostsData = () => {
-    const [posts, setPosts] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const { data: posts, error, isLoading } = useSWR('http://localhost:3000/posts', axiosFetch, {
+        dedupingInterval: 1000 * 60 * 10, // cache for 10 minutes});
+    });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://localhost:3000/posts/`);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const result = await response.json();
-                setPosts(result);
-            } catch (error) {
-                setError(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return { posts, error, loading };
+    return { posts, error, isLoading };
 };
 
 const Posts = () => {
-    const { posts, error, loading } = usePostsData();
+    const { posts, error, isLoading } = usePostsData();
 
-    if (loading) return <p>Loading...</p>;
+    if (isLoading) return <p>Loading...</p>;
     if (error) return <p>A network error was encountered</p>;
-
-    console.log(posts);
 
     return (
         <div className="mt-5">
-            <h2 className="text-2xl text-center font-bold underline2">Posts</h2>
+            <h2 className="text-2xl font-bold mb-3">All Posts</h2>
             <ul>
                 {posts.data.map((post) => (
-                    <li key={post._id}>
+                    <Link key={post._id} to={`/posts/${post._id}`}>
+                        <li className='border mb-2 p-3 rounded hover:bg-slate-200'>
+                            <h3 className='inline-block text-lg font-bold'>{post.title}&nbsp;</h3>
+                            <h3 className='inline-block text-md text-zinc-400'>- {post.author.fullname}</h3>
+                            <p>{post.text}</p>
+                            <p className='text-xs italic inline-block'>{DateTime.now().toUTC().plus(DateTime.fromISO(post.createdAt).diffNow()).toRelative()}</p>
+                            {post.updatedAt !== post.createdAt && (
+                                <p className='text-xs italic inline-block pl-5'>Modified: {DateTime.now().toUTC().plus(DateTime.fromISO(post.updatedAt).diffNow()).toRelative()}</p>
+                            )}
+                        </li>
+                    </Link>
 
-                    </li>
                 ))}
             </ul>
         </div>
