b175038b98bffc3f0e357e2234fd0bab52e27b5f
alex-delia <adelia3@uwo.ca>
2024-07-11 13:40:11 -0400

    create context to manage if user is authenticated

---
+++ frontend-user/src/context/AuthContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+
+const AuthContext = createContext();
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setIsAuthenticated(true);
+            setUser(JSON.parse(localStorage.getItem('user')));
+        }
+    }, []);
+
+    const login = (userData, token) => {
+        setIsAuthenticated(true);
+        setUser(userData);
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(userData));
+        toast.success('Log In Success');
+    };
+
+    const logout = () => {
+        setIsAuthenticated(false);
+        setUser(null);
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        toast.success('Log Out Success');
+    };
+
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
