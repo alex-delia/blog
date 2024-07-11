59275885bf233cf8349860ac39028d633bfd854e
alex-delia <adelia3@uwo.ca>
2024-07-11 13:53:41 -0400

    create a toast container for default notifications

--- frontend-user/src/App.jsx
+++ frontend-user/src/App.jsx
@@ -1,5 +1,7 @@
 import NavBar from './components/layout/NavBar.jsx';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer, Flip } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -7,6 +9,20 @@ function App() {
     <div className='min-h-screen lg:px-40 md:px-20 px-10'>
       <NavBar />
       <Outlet />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        limit={5}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+        transition={Flip}
+      />
     </div>
   );
 }
