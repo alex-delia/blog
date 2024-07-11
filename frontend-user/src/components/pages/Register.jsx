05bc5dae27f9b0f0a741bae50f37c2b1cac96d5b
alex-delia <adelia3@uwo.ca>
2024-07-11 14:19:12 -0400

    protect the register route against user auth

--- frontend-user/src/components/pages/Register.jsx
+++ frontend-user/src/components/pages/Register.jsx
@@ -1,4 +1,14 @@
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
 const Register = () => {
+    const { isAuthenticated } = useContext(AuthContext);
+
+    if (isAuthenticated) {
+        return <Navigate to={'/'} replace />;
+    }
+
     return (
         <div className="mt-5">
             <h2 className="text-2xl text-center font-bold underline2">Register</h2>
