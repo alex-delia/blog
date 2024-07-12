51ec260b1bdeddaea5f28e5c6f47ac21c7f4c889
alex-delia <adelia3@uwo.ca>
2024-07-12 14:35:28 -0400

    move create user router to /register

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function (req, res, next) {
   res.send('GET request for index page');
 });
 
+//POST request for logging in
 router.post('/login', user_controller.login);
+
+//POST request for creating user
+router.post('/register', user_controller.user_create);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,9 +4,6 @@ const router = express.Router();
 const user_controller = require('../controllers/userController');
 const { authenticateJWT, deleteUserAuthorization } = require('../middlewares/authMiddleware');
 
-//POST request for creating user
-router.post('/', user_controller.user_create);
-
 //DELETE request for deleting user
 router.delete('/:userId', authenticateJWT, deleteUserAuthorization, user_controller.user_delete);
 
