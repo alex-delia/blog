bbee4271937284257aff3a4f66da1a1b9e2c3319
alex-delia <adelia3@uwo.ca>
2024-07-12 14:42:10 -0400

    include 'he' library for decoding names from DB

--- frontend-user/src/components/common/PostPreview.jsx
+++ frontend-user/src/components/common/PostPreview.jsx
@@ -1,5 +1,6 @@
 import convertUTCToUserTimeZone from '../../helpers/convertUTCtoLocal';
 import { DateTime } from "luxon";
+import he from 'he';
 
 /* eslint-disable react/prop-types */
 const PostPreview = ({ post }) => {
@@ -8,7 +9,7 @@ const PostPreview = ({ post }) => {
     return (
         <div className='shadow-md rounded-md p-4 h-full hover:bg-slate-200 '>
             <h3 className='text-lg font-bold'>{post.title}&nbsp;</h3>
-            <h4 className='text-sm text-zinc-400'>{post.author.fullname}</h4>
+            <h4 className='text-sm text-zinc-400'>{he.decode(post.author.fullname)}</h4>
             <p className='text-xs italic mb-1'>
                 {post.updatedAt === post.createdAt ?
                     convertUTCToUserTimeZone(DateTime.fromISO(post.createdAt), userTimeZone).toLocaleString(DateTime.DATE_FULL) :
