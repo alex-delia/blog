4cacf9376623d61d0f9d80540d9f9508997c3589
alex-delia <adelia3@uwo.ca>
2024-07-13 14:37:36 -0400

    initial commit for author frontend page

---
+++ frontend-author/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className='min-h-screen lg:px-40 md:px-20 px-10 pb-20'>
+      <h1 className='text-3xl mt-5 font-bold'>Author Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
