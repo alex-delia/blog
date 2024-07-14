aa3afcc5fe89a00af6ba9963a9b8aac70181ae3d
alex-delia <adelia3@uwo.ca>
2024-07-13 21:07:30 -0400

    include toastcontainer for notifications

--- frontend-author/src/main.jsx
+++ frontend-author/src/main.jsx
@@ -5,6 +5,8 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
 import ErrorPage from './components/errors/Error.jsx';
 import Login from './components/pages/Login.jsx';
+import { ToastContainer, Flip } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -24,6 +26,20 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={4000}
+        limit={5}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="colored"
+        transition={Flip}
+      />
     </AuthProvider>
   </React.StrictMode>,
 );
