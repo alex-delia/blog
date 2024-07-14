2147ead5cd300cc98812b364f7459a457e2da4fc
alex-delia <adelia3@uwo.ca>
2024-07-13 21:09:16 -0400

    use custom button component for logout

--- frontend-author/src/App.jsx
+++ frontend-author/src/App.jsx
@@ -1,10 +1,10 @@
-import { ToastContainer, Flip } from "react-toastify";
 import { Outlet, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "./context/AuthContext";
+import Button from "./components/common/Button";
 
 function App() {
-  const { isAuthenticated } = useContext(AuthContext);
+  const { isAuthenticated, logout } = useContext(AuthContext);
 
   if (!isAuthenticated) {
     return <Navigate to='login' replace />;
@@ -12,23 +12,11 @@ function App() {
 
   return (
     <div className='min-h-screen lg:px-40 md:px-20 px-10 pb-20'>
-      <h1 className='text-3xl mt-5 font-bold'>Author Dashboard</h1>
-
+      <div className="flex items-center justify-between mt-5">
+        <h1 className='text-3xl font-bold'>Author Dashboard</h1>
+        <Button text='Logout' onClick={logout} bgColor="bg-red-500" hoverColor="bg-red-400" />
+      </div>
       <Outlet />
-      <ToastContainer
-        position="bottom-center"
-        autoClose={3000}
-        limit={5}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover={false}
-        theme="colored"
-        transition={Flip}
-      />
     </div>
   );
 }
