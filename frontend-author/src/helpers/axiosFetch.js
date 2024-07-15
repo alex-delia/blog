71584ec267998c49ea0cb07bf78c2cf1e46a759c
alex-delia <adelia3@uwo.ca>
2024-07-15 16:17:29 -0400

    attach the user to the req for posts

--- frontend-author/src/helpers/axiosFetch.js
+++ frontend-author/src/helpers/axiosFetch.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const axiosFetch = async (url) => {
-    const response = await axios.get(url);
+export const axiosFetch = async (url, config) => {
+    const response = await axios.get(url, config);
     return response.data;
 }; 
--- frontend-author/src/helpers/usePostsData.js
+++ frontend-author/src/helpers/usePostsData.js
@@ -2,9 +2,15 @@ import useSWR from 'swr';
 import { axiosFetch } from './axiosFetch';
 
 const usePostsData = (authorId) => {
-    const { data, error, isLoading } = useSWR(`http://localhost:3000/authors/${authorId}/posts`, axiosFetch, {
-        dedupingInterval: 1000 * 60 * 10, // cache for 10 minutes});
-    });
+    const config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    };
+
+    const { data, error, isLoading } = useSWR(`http://localhost:3000/authors/${authorId}/posts`,
+        (url) => axiosFetch(url, config),
+        {
+            dedupingInterval: 1000 * 60 * 10, // cache for 10 minutes});
+        });
 
     return { postsData: data, error, isLoading };
 };
