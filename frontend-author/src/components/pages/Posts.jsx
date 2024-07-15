6b2024530228c635893947a576a3e2c0eb0acfb3
alex-delia <adelia3@uwo.ca>
2024-07-15 17:31:06 -0400

    allow author to publish/unpublish posts

--- frontend-author/src/components/pages/Posts.jsx
+++ frontend-author/src/components/pages/Posts.jsx
@@ -1,13 +1,19 @@
 import usePostsData from "../../helpers/usePostsData";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import DOMPurify from "dompurify";
 import he from 'he';
+import Button from "../common/Button";
+import axios from "axios";
+import { useSWRConfig } from "swr";
+
 
 export default function Posts() {
-    const { isAuthenticated, loading, user } = useContext(AuthContext);
+    const { mutate } = useSWRConfig();
+    const navigate = useNavigate();
 
+    const { isAuthenticated, loading, user } = useContext(AuthContext);
     const { postsData, error, isLoading } = usePostsData(user.id);
 
     if (loading || isLoading) {
@@ -23,20 +29,42 @@ export default function Posts() {
     postsData.posts.forEach((post) => {
         // Decode the HTML entities
         const decodedTitle = he.decode(post.title);
-
         // Sanitize the decoded HTML
         const sanitizedTitle = DOMPurify.sanitize(decodedTitle);
-
         post.title = sanitizedTitle;
     });
+
+    const config = {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    };
+
+    const togglePublish = async (post) => {
+        try {
+            const response = await axios.put(`${import.meta.env.VITE_BACKEND_URL}/posts/${post.id}`,
+                { isPublished: !post.isPublished },
+                config
+            );
+            mutate(`http://localhost:3000/authors/${user.id}/posts`);
+            navigate('/posts');
+            console.log(response.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
     return (
         <div className="mt-5">
             <h1 className="text-2xl font-bold mb-4">Posts</h1>
             {postsData.posts.map((post) => (
-                <div key={post.id} className="py-4 border-b">
-                    <h2 className="font-bold">{post.title}</h2>
-                    <p>{post.description}</p>
+                <div key={post.id} className="flex items-center justify-between py-4 border-b">
+                    <div>
+                        <h2 className="font-bold">{post.title}</h2>
+                        <p>{post.description}</p>
+                    </div>
+                    {post.isPublished ?
+                        <Button text="Unpublish" bgColor="bg-red-500" hoverColor="hover:bg-red-400" onClick={() => togglePublish(post)} />
+                        : <Button text="Publish" bgColor="bg-green-600" hoverColor="hover:bg-green-500" onClick={() => togglePublish(post)} />
+                    }
                 </div>
             ))}
         </div>
