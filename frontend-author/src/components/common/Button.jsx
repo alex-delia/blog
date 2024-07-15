36206c9c9e2c083416caf438a05e93a736838dfc
alex-delia <adelia3@uwo.ca>
2024-07-14 23:06:50 -0400

    fix button hover styling

--- frontend-author/src/components/common/Button.jsx
+++ frontend-author/src/components/common/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Button = ({ text, onClick, bgColor = 'bg-indigo-600', hoverColor = 'hover:bg-indigo-500' }) => {
     return (
-        <button onClick={onClick} className={`text-lg ${bgColor} py-2 px-4 text-white rounded-2xl hover:${hoverColor}`}>
+        <button onClick={onClick} className={`text-lg ${bgColor} py-2 px-4 text-white rounded-2xl ${hoverColor}`}>
             {text}
         </button>
     );
