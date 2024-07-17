defa5222f2514d31c2df56d13463e9cd8fba0745
alex-delia <adelia3@uwo.ca>
2024-07-17 16:21:42 -0400

    attach the token to all requests if it exists

--- frontend-user/src/helpers/fetch.js
+++ frontend-user/src/helpers/fetch.js
@@ -1,6 +1,16 @@
 import axios from 'axios';
 
 export const axiosFetch = async (url) => {
-    const response = await axios.get(url);
+    const token = localStorage.getItem('token');
+    let config;
+
+    if (token) {
+        console.log(token);
+        config = {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        };
+    }
+
+    const response = await axios.get(url, config);
     return response.data;
 }; 
