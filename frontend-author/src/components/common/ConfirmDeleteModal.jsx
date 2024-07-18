ebd5040d44c9e84b8172404b1f8e7d23c0663c6f
alex-delia <adelia3@uwo.ca>
2024-07-18 18:51:45 -0400

    make deletion confirmation more detailed

--- frontend-author/src/components/common/ConfirmDeleteModal.jsx
+++ frontend-author/src/components/common/ConfirmDeleteModal.jsx
@@ -5,7 +5,9 @@ const ConfirmDeleteModal = ({ onConfirm, onCancel }) => {
         <div className="fixed inset-0 z-10 bg-opacity-80 bg-gray-800 flex justify-center items-center">
             <div className="bg-white rounded-lg p-6 shadow-lg">
                 <h2 className="text-xl font-semibold mb-4">Are you sure?</h2>
-                <p className="mb-4">Do you really want to delete this item? This process cannot be undone.</p>
+                <p className="mb-4">Do you really want to delete this post? </p>
+                <p className='mb-4'>All comments associated with this post will also be deleted.</p>
+                <p className='mb-4'>This process cannot be undone.</p>
                 <div className="flex justify-end space-x-4">
                     <button
                         className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
