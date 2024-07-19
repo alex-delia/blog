f434334722f54fa3dfe9fc923f8cb9c8991a1774
alex-delia <adelia3@uwo.ca>
2024-07-19 00:19:15 -0400

    initialize sentry

--- frontend-author/src/main.jsx
+++ frontend-author/src/main.jsx
@@ -16,6 +16,23 @@ import EditPostForm from './components/pages/EditPostForm.jsx';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://b147175f145a1e40e27a56347b4a991b@o4507626512580608.ingest.us.sentry.io/4507626540236800",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 const router = createBrowserRouter([
   {
     path: '/',
